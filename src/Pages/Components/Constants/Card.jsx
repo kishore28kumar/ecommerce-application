37b476a5d309ec8724e33e1d3096ed80ee1aa912
kishore28kumar <kishore28kks@gmail.com>
2024-02-13 12:12:39 +0530

    Updated Files

--- src/Pages/Components/Constants/Card.jsx
+++ src/Pages/Components/Constants/Card.jsx
@@ -1,19 +1,226 @@
-import React from 'react'
-
+import React from "react";
+import greyheart from "./img/greyheart.jpg";
+import cartplus from "./img/cartplus.png";
 const Card = () => {
   return (
-    <div className='cardContainer'>
+    <section className="p">
+      <div className="cardContainer">
         <div className="cardWrapper">
-            <img src="/" alt="product-img" />
-            <div className="bottomCard">
-                <h4>EYEBOGLER Regular Fit Men's Cotton T-Shirt</h4>
-                <p>200</p>
-                <span>4.0</span>
-                <img src="" alt="heart" />
+          <img
+            className="product"
+            src="https://i.pinimg.com/originals/25/0d/a3/250da32c889b624849bf828ae2ece420.jpg"
+            alt="product-img"
+          />
+          <div className="bottomCard">
+            <p>T-shirt for formal Men</p>
+            <p>₹ 200</p>
+            <h5>4.0</h5>
+            <div className="bottom">
+              <img src={greyheart} alt="heart" />
+              <img className="cart" src={cartplus} alt="cart" />
             </div>
+          </div>
         </div>
-    </div>
-  )
-}
-
-export default Card
+      </div>
+      <div className="cardContainer">
+        <div className="cardWrapper">
+          <img
+            className="product"
+            src="https://i.pinimg.com/originals/25/0d/a3/250da32c889b624849bf828ae2ece420.jpg"
+            alt="product-img"
+          />
+          <div className="bottomCard">
+            <p>T-shirt for formal Men</p>
+            <p>₹ 200</p>
+            <h5>4.0</h5>
+            <div className="bottom">
+              <img src={greyheart} alt="heart" />
+              <img className="cart" src={cartplus} alt="cart" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cardContainer">
+        <div className="cardWrapper">
+          <img
+            className="product"
+            src="https://i.pinimg.com/originals/25/0d/a3/250da32c889b624849bf828ae2ece420.jpg"
+            alt="product-img"
+          />
+          <div className="bottomCard">
+            <p>T-shirt for formal Men</p>
+            <p>₹ 200</p>
+            <h5>4.0</h5>
+            <div className="bottom">
+              <img src={greyheart} alt="heart" />
+              <img className="cart" src={cartplus} alt="cart" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cardContainer">
+        <div className="cardWrapper">
+          <img
+            className="product"
+            src="https://i.pinimg.com/originals/25/0d/a3/250da32c889b624849bf828ae2ece420.jpg"
+            alt="product-img"
+          />
+          <div className="bottomCard">
+            <p>T-shirt for formal Men</p>
+            <p>₹ 200</p>
+            <h5>4.0</h5>
+            <div className="bottom">
+              <img src={greyheart} alt="heart" />
+              <img className="cart" src={cartplus} alt="cart" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cardContainer">
+        <div className="cardWrapper">
+          <img
+            className="product"
+            src="https://i.pinimg.com/originals/25/0d/a3/250da32c889b624849bf828ae2ece420.jpg"
+            alt="product-img"
+          />
+          <div className="bottomCard">
+            <p>T-shirt for formal Men</p>
+            <p>₹ 200</p>
+            <h5>4.0</h5>
+            <div className="bottom">
+              <img src={greyheart} alt="heart" />
+              <img className="cart" src={cartplus} alt="cart" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cardContainer">
+        <div className="cardWrapper">
+          <img
+            className="product"
+            src="https://i.pinimg.com/originals/25/0d/a3/250da32c889b624849bf828ae2ece420.jpg"
+            alt="product-img"
+          />
+          <div className="bottomCard">
+            <p>T-shirt for formal Men</p>
+            <p>₹ 200</p>
+            <h5>4.0</h5>
+            <div className="bottom">
+              <img src={greyheart} alt="heart" />
+              <img className="cart" src={cartplus} alt="cart" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cardContainer">
+        <div className="cardWrapper">
+          <img
+            className="product"
+            src="https://i.pinimg.com/originals/25/0d/a3/250da32c889b624849bf828ae2ece420.jpg"
+            alt="product-img"
+          />
+          <div className="bottomCard">
+            <p>T-shirt for formal Men</p>
+            <p>₹ 200</p>
+            <h5>4.0</h5>
+            <div className="bottom">
+              <img src={greyheart} alt="heart" />
+              <img className="cart" src={cartplus} alt="cart" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cardContainer">
+        <div className="cardWrapper">
+          <img
+            className="product"
+            src="https://i.pinimg.com/originals/25/0d/a3/250da32c889b624849bf828ae2ece420.jpg"
+            alt="product-img"
+          />
+          <div className="bottomCard">
+            <p>T-shirt for formal Men</p>
+            <p>₹ 200</p>
+            <h5>4.0</h5>
+            <div className="bottom">
+              <img src={greyheart} alt="heart" />
+              <img className="cart" src={cartplus} alt="cart" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cardContainer">
+        <div className="cardWrapper">
+          <img
+            className="product"
+            src="https://i.pinimg.com/originals/25/0d/a3/250da32c889b624849bf828ae2ece420.jpg"
+            alt="product-img"
+          />
+          <div className="bottomCard">
+            <p>T-shirt for formal Men</p>
+            <p>₹ 200</p>
+            <h5>4.0</h5>
+            <div className="bottom">
+              <img src={greyheart} alt="heart" />
+              <img className="cart" src={cartplus} alt="cart" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cardContainer">
+        <div className="cardWrapper">
+          <img
+            className="product"
+            src="https://i.pinimg.com/originals/25/0d/a3/250da32c889b624849bf828ae2ece420.jpg"
+            alt="product-img"
+          />
+          <div className="bottomCard">
+            <p>T-shirt for formal Men</p>
+            <p>₹ 200</p>
+            <h5>4.0</h5>
+            <div className="bottom">
+              <img src={greyheart} alt="heart" />
+              <img className="cart" src={cartplus} alt="cart" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cardContainer">
+        <div className="cardWrapper">
+          <img
+            className="product"
+            src="https://i.pinimg.com/originals/25/0d/a3/250da32c889b624849bf828ae2ece420.jpg"
+            alt="product-img"
+          />
+          <div className="bottomCard">
+            <p>T-shirt for formal Men</p>
+            <p>₹ 200</p>
+            <h5>4.0</h5>
+            <div className="bottom">
+              <img src={greyheart} alt="heart" />
+              <img className="cart" src={cartplus} alt="cart" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cardContainer">
+        <div className="cardWrapper">
+          <img
+            className="product"
+            src="https://i.pinimg.com/originals/25/0d/a3/250da32c889b624849bf828ae2ece420.jpg"
+            alt="product-img"
+          />
+          <div className="bottomCard">
+            <p>T-shirt for formal Men</p>
+            <p>₹ 200</p>
+            <h5>4.0</h5>
+            <div className="bottom">
+              <img src={greyheart} alt="heart" />
+              <img className="cart" src={cartplus} alt="cart" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Card;
