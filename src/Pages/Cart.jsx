8223dc652e88b8799236c3ed5f83ab10b8c82e45
kishore28kumar <kishore28kks@gmail.com>
2024-02-18 18:53:08 +0530

    Updated Files

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,10 +1,14 @@
 import Nav from "./Components/Constants/Nav";
 import Card2 from "./Components/Constants/Card2";
+import Orderp from "./Components/Constants/Orderp";
 const Cart = () => {
   return (
     <section className="cart">
-      <Nav />
-      <Card2 />
+      <div className="ops">
+        <Nav />
+        <Card2 />
+      </div>
+      <Orderp />
     </section>
   );
 };
