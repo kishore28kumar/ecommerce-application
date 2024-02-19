8a8596af92eba9d64ecf53e5c092f4a84d2b3e40
kishore28kumar <kishore28kks@gmail.com>
2024-02-19 20:45:01 +0530

    Updated Files

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -2,6 +2,16 @@ import Nav from "./Components/Constants/Nav";
 import Card2 from "./Components/Constants/Card2";
 import Orderp from "./Components/Constants/Orderp";
 const Cart = () => {
+  // const opl = document.getElementById("opl"),
+  //   oplBtn = document.getElementById("bot"),
+  //   oplclose = document.getElementById("cart");
+
+  // oplBtn.addEventListener("click", () => {
+  //   opl.classList.add(".show-opl");
+  // });
+  // oplclose.addEventListener("click", () => {
+  //   opl.classList.remove(".show-opl");
+  // });
   return (
     <section className="cart">
       <div className="ops">
